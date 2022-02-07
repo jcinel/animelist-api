203b459939f348ec445194215b869b0ae073449f
jcinel <julia.cinel@estudante.ufscar.br>
2022-02-07 10:37:31 -0300

    Base do projeto

---
+++ src/test/java/com/crud/animelist/AnimeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.animelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
