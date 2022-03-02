cf6d843b62bceef186ec754207fac6352eecf6b4
jcinel <julia.cinel@estudante.ufscar.br>
2022-03-02 13:35:21 -0300

    versão inicial do projeto

--- src/main/java/com/api/animelist/models/ListaAnimesKey.java
+++ src/main/java/com/api/animelist/models/ListaAnimesKey.java
@@ -3,7 +3,6 @@ package com.api.animelist.models;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
-import java.util.List;
 import java.util.Objects;
 
 @Embeddable
